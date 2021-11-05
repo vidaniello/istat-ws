e92948c82ebc66e41c83acfce242620dd7fb363f
Vincenzo D'Aniello <vidaniello@gmail.com>
2021-11-05 16:03:35 +0100

    Refactoring

--- server.js
+++ server.js
@@ -2,20 +2,16 @@
 
 const pubsub = require('pubsub-js');
 const common = require('./common');
+const downloader = require('./downloader');
+const database = require('./database');
 const httpServer = require('./httpServer');
 const chron = require('./chron');
 
+common.init();
+chron.init();
+httpServer.initHttpServer();
 
-function init(){
 
-    common.init();
-
-    for(let src of common.allDataSource){
-        pubsub.publish(common.CHECK_ETA_TOPIC, src);
-    }
-    chron.init();
-    httpServer.initHttpServer();
+for(let src of common.allDataSource){
+    pubsub.publish(common.CHECK_ETA_TOPIC, src);
 }
-module.exports.init = init;
-
-init();
